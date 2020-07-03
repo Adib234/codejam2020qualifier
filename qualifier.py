22adda5ee6c0b3ba4bb3a61af50f628a1d9863d3
Adib234 <adibadib42@hotmail.com>
2020-07-02 22:35:32 -0400

    First commit

--- qualifier.py
+++ qualifier.py
@@ -15,17 +15,80 @@ Important notes for submission:
 """
 import datetime
 import typing
+from collections import Counter
+import itertools
+import gc
 
 
-class ArticleField:
+class ArticleField():
     """The `ArticleField` class for the Advanced Requirements."""
 
-    def __init__(self, field_type: typing.Type[typing.Any]):
-        pass
 
-
-class Article:
+class Article(ArticleField):
     """The `Article` class you need to write for the qualifier."""
+    newid = itertools.count()
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.content = content
+        self.id = next(self.newid)
+        self._last_edited = None
+
+    @property
+    def last_edited(self):
+        return self._last_edited
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, val):
+        self._content = val
+        self._last_edited = datetime.datetime.now()
+
+    def __repr__(self):
+        return f'<Article title=\"{self.title}\" author=\'{self.author}\' publication_date=\'{ self.publication_date.isoformat()}\'>'
+
+    def __len__(self):
+        return len(self.content)
+
+    def __lt__(self, other):
+        return self.publication_date < other.publication_date
+
+    def __sorted__(self):
+        for passnum in range(len(self)-1, 0, -1):
+            for i in range(passnum):
+                if self[i].publication_date > self[i+1].publication_date:
+                    temp = self[i].publication_date
+                    self[i].publication_date = self[i+1].publication_date
+                    self[i+1].publication_date = temp
+        return repr(self)
+
+    def short_introduction(self, n_characters: int):
+        if self.content[n_characters].isalpha() == True:
+            start = n_characters-1
+            while self.content[start].isalpha() == True:
+                start -= 1
+            return self.content[:start+1].strip()
+        else:
+            return self.content[:n_characters].strip()
+
+    def most_common_words(self, n):
+        for i in range(len(self.content)):
+            if self.content[i].isalpha() == False and self.content[i] != '':
+                self.content = self.content.replace(self.content[i], ' ')
+        commonList = self.content.replace('\n', ' ').lower().split()
+        counter = Counter(commonList).most_common(n)
+
+        order = []
+        for i in counter:
+            order.append(i[0])
+
+        final = {}
+
+        for i in range(len(counter)):
+            final[order[i]] = counter[i][1]
+        return final
